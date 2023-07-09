b21045afd83811e0dcd0680399d826db2fdb98d7
diwak <diwak@DESKTOP-PUS1071>
2023-07-09 19:20:09 +0530

    First Commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+	int n1=5;
+	int n2=2;
+	public void add() {
+		int c=n1+n2;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		new Calculator().add();
+	}
+
+}
