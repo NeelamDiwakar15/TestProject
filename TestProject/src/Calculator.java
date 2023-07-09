6e07c2a25770db989686be54d5ae6008115fd700
diwak <diwak@DESKTOP-PUS1071>
2023-07-09 20:08:46 +0530

    Second commit

--- TestProject/src/Calculator.java
+++ TestProject/src/Calculator.java
@@ -6,9 +6,14 @@ public class Calculator {
 		int c=n1+n2;
 		System.out.println(c);
 	}
+	public void product() {
+		int c=n1*n2;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		new Calculator().add();
+		
 	}
 
 }
